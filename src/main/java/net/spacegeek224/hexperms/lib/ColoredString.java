9efb6957825557f12d54292ee53a1f293e8a853d
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-02-07 12:06:03 -0500

    Update ColoredString.java

--- src/main/java/net/spacegeek224/hexperms/lib/ColoredString.java
+++ src/main/java/net/spacegeek224/hexperms/lib/ColoredString.java
@@ -1,5 +1,10 @@
 package net.spacegeek224.hexperms;
 
 public class ColoredString {
-
+  public String original = "";
+  public String res = "";
+  public ColoredString(String s) {
+    original = s;
+    res = ChatColor.translateAlternateColorCodes('&',s);
+  }
 }
